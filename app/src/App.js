8fc1065124850efacac5bf218d03292568d2822c
jfersonREI <96252197+jfersonREI@users.noreply.github.com>
2024-07-04 02:21:55 -0400

    sass

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,11 @@ function App() {
     <div className="App">
       <Container>
         <LandingWrapper>
-          <LandingDetails>ddd</LandingDetails>
+          <LandingDetails
+            subtitle="Subtitle"
+            title="Assess your project’s CX maturity level"
+            copy="Each year, we engage with project managers to gain valuable insights into project performance, specifically focusing on customer experience (CX). Our project maturity assessment process ensures that every REI project is on the right track. Let’s elevate project success together!"
+          ></LandingDetails>
           <LandingCover></LandingCover>
         </LandingWrapper>
       </Container>
--- app/src/components/Layout/LandingDetails.js
+++ app/src/components/Layout/LandingDetails.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./LandingDetails.scss";
+import logo from "../../assets/logo-rei.svg";
 
 const LandingDetails = (props) => {
-  return <div className="landing-details">{props.children}</div>;
+  return (
+    <div className="landing-details">
+      <div className="landing-details__header">
+        <img src={logo} className="landing-details__logo" alt="REI" />
+      </div>
+      <div className="landing-details__body">
+        <div className="landing-details__body-header">
+          <p className="landing-details__subtitle">{props.subtitle}</p>
+          <p className="landing-details__title">{props.title}</p>
+        </div>
+        <div className="landing-details__copy">{props.copy}</div>
+      </div>
+
+      {props.children}
+    </div>
+  );
 };
 
 export default LandingDetails;
